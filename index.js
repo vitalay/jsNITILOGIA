c95b655db6d768eef96c5fb9c8b45c815dc0ca68
vitalya <vitalay.efimov@gmail.com>
2025-08-21 20:15:33 +0500

    2 task

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 let username = "LeoTolstoy";
 let bonusBalance = 131;
+let points = 50
+let burningPoints = 3
+
 console.log('Пользователь: ' + username);
 console.log('Баланс: ' + bonusBalance);
+
+console.log(`Баланс:   ${bonusBalance + (points * 10 / 2 - 3 * 10) }`);
+
