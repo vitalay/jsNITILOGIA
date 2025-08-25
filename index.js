83912e7102e5d75529b9ec483448d218eb26bffc
vitalya <vitalay.efimov@gmail.com>
2025-08-25 20:27:48 +0500

    task2

--- index.js
+++ index.js
@@ -86,3 +86,13 @@ if (name === 'admin'){
 } else {
   console.log("Обычный пользователь");
 }
+
+
+let user = 'netologist'
+let orderOwner = 'leo_tolstoy'
+
+if (user === orderOwner || user === 'admin'){
+  console.log("Редактирование разрешено");
+}else {
+  console.log("Заказ нельзя редактировать");
+}
